565f1c5622da8d0c35b1317a2e5c3954736c706a
邓 顺明 <1156477385@qq.com>
2025-03-20 13:01:07 +0000

    增加查询参数

--- pages/NIFI Flow Overview/jsobjects/pageHandler/pageHandler.js
+++ pages/NIFI Flow Overview/jsobjects/pageHandler/pageHandler.js
@@ -4,6 +4,16 @@ export default {
 	log_select.run();
 },
  	onPageNoOrPageSizeChange_config: ()=>{
-	config_select.run();
+	log_select.run();
+},
+faillog_search: ()=>{
+		storeValue('searchtxt','fail').then(()=>{
+				log_select.run();
+		});
+},
+successlog_search: ()=>{
+		storeValue('searchtxt','success').then(	() => {
+     log_select.run();
+    });
 }
 }
